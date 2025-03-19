a461b9e81f0f010138ba508021d1804728e2bc36
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2025-03-19 17:54:18 +0300

    file structure
    file structure

---
+++ app/(Admin)/Admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1> admin Dashboard </h1>
+    </div>
+  )
+}
---
+++ app/(Supervisor)/Supervisor/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1>Supervisor Dashboard</h1>
+    </div>
+  )
+}
